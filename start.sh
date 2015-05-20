b83e964864f94ed708854b890221f23009583aa9
Marian Schwarz <scorp@sblk.de>
2015-05-20 20:02:07 +0200

    -- modified start.sh

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U nfld
 sed -i "s/host\s=\s.*/host = $POSTGRES_PORT_5432_TCP_ADDR/g" /root/.config/nfldb/config.ini
 sed -i "s/port\s=\s.*/port = $POSTGRES_PORT_5432_TCP_PORT/g" /root/.config/nfldb/config.ini
 
-nfldb-update --interval 30
+nfldb-update --interval 120
